30354ef9c61874205268102f2acbbe8826a891c3
lsroshini <22pw23@psgtech.ac.in>
2025-04-21 20:07:27 +0530

    initial commit

---
+++ TaxQueen-main/frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+      <div className="items-center grid-flow-col">
+      </div>
+    </footer>
+  );
+}
